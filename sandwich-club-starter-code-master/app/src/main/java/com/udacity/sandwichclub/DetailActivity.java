61778b47123ff347b217753922507920aaeb231d
Mustafa Shaik <symadept@gmail.com>
2018-04-30 16:16:16 -0500

    Implemented Databinding, Scrollview etc

--- sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,26 +1,37 @@
 package com.udacity.sandwichclub;
 
+import android.content.Context;
 import android.content.Intent;
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private ActivityDetailBinding mBindings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        mBindings = DataBindingUtil.setContentView(this, R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +54,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(mBindings.imageIv);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +67,21 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        mBindings.setSandwich(sandwich);
 
+        if(sandwich.getAlsoKnownAs() == null || sandwich.getAlsoKnownAs().size()<1) {
+            mBindings.alsoKnownAsTv.setText(" ");
+        } else {
+            String commaSeparatedItems = TextUtils.join(",", sandwich.getAlsoKnownAs());
+            mBindings.alsoKnownAsTv.setText(commaSeparatedItems);
+        }
+
+        if(sandwich.getIngredients() == null || sandwich.getIngredients().size()<1) {
+            mBindings.ingredientsTv.setText(" ");
+        } else {
+            String commaSeparatedItems = "* " + TextUtils.join("\n* ", sandwich.getIngredients());
+            mBindings.ingredientsTv.setText(commaSeparatedItems);
+        }
     }
 }
--- sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,54 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        if(json == null) return null;
+        Sandwich sandwich = null;
+        try {
+            JSONObject sandwichObject = new JSONObject(json);
+            JSONObject nameObject = sandwichObject.getJSONObject("name");
+            sandwich = new Sandwich();
+
+            if(null != nameObject) {
+                sandwich.setMainName(nameObject.getString("mainName"));
+
+                List<String> alsoKnownAs = toStringList(nameObject.getJSONArray("alsoKnownAs"));
+                sandwich.setAlsoKnownAs(alsoKnownAs);
+            }
+
+            sandwich.setPlaceOfOrigin(sandwichObject.getString("placeOfOrigin"));
+            sandwich.setDescription(sandwichObject.getString("description"));
+            sandwich.setImage(sandwichObject.getString("image"));
+
+            List<String> ingredients = toStringList(sandwichObject.getJSONArray("ingredients"));
+            sandwich.setIngredients(ingredients);
+
+        } catch (final JSONException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return sandwich;
+    }
+
+    private static List<String> toStringList(JSONArray array) throws JSONException{
+        List<String> listdata = new ArrayList<String>();
+        if (array != null) {
+            for (int i=0;i<array.length();i++){
+                listdata.add(array.getString(i));
+            }
+        }
+        return listdata;
     }
 }
